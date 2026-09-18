bba5afd6366b8d91fd805175b1800d733f61af76
tb <taobo0505@icloud.com>
2026-09-18 10:15:24 +0800

    Initial Commit

---
+++ ddlNew/ddlNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddlNewApp.swift
+//  ddlNew
+//
+//  Created by taobo on 2026/9/18.
+//
+
+import SwiftUI
+
+@main
+struct ddlNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
